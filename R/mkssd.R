35841d4b9f422dfa75a363f2023774010897fc71
B N Mandal <mandal.stat@gmail.com>
2011-08-04 00:00:00 +0000

    version 1.1

--- R/mkssd.R
+++ R/mkssd.R
@@ -3,13 +3,10 @@
 #q=number of levels
 #D is the supersaturated design
 
-genvec=function(m,n,q,k)
-#The function created initial k-circulant q-level SSD randomly.
+genmat=function(m,n,q,k)
+#The function creates generator matrix of initial k-circulant q-level SSD randomly.
 {
-	t=n/q	#checks condition 1
-	if (n%%q !=0) stop("n should be divisible by q")
-	if (m!=(n-1)*k) stop("k-circulant design not possible") #checks condition 1	
-	gvec=matrix(0,1,m)
+	t=n/q	
 	gmat=matrix(0,n-1,k)
 	for (i in 1:(q-1))
 	{
@@ -26,6 +23,13 @@ genvec=function(m,n,q,k)
 	{
 		gmat[,i]=sample(gmat[,1],(n-1))
 	}	#condition 2
+	gmat
+}
+
+
+gmat_to_gvec=function(gmat,n,k)
+{
+	gvec=matrix(0,1,(n-1)*k)  # since m=(n-1)k
 	count=0
 	for (j in 1:(n-1))
 	{
@@ -38,9 +42,10 @@ genvec=function(m,n,q,k)
 	gvec
 }
 
-D_from_G=function(genvec,m,n,q,k)
+D_from_G=function(gmat,m,n,q,k)
 {
 #the function creates the design obtained by k-circulating the generator vector
+	genvec=gmat_to_gvec(gmat,n,k)
 	D=matrix(0,n,m)
 	D[1,]=genvec
 	#k-circulating the first row to next (n-2) rows
@@ -117,15 +122,20 @@ mkssd=function(m,n,q,k,mef)
 #After full iteration, design may not have chisquare efficiency 1. In that case recall the function. mef is the minimum efficiency required, should be between 0 to 1.
 	#time of execution
 	stime=proc.time()
+	t=n/q
+	if (n%%q !=0) stop("n should be divisible by q")
+	if (m!=(n-1)*k) stop("k-circulant design not possible. Parametric conditions not satisfied.") #checks condition 1
+	if (m>factorial(n-1)/(factorial(q-1)*factorial(t-1)*(factorial(t))**(q-1))) stop("design with non-aliased pair of columns do not exist")	
 	L_chisq=(q-1)*n*((q-1)*m-n+1)/((n-1)*(m-1))
 	aliased=100000
 	trial=0
-	while (aliased>0 && trial<=100)  #aliased=1 means two columns in the design are fully aliased
+	while (aliased>0 && trial<=100)  #aliased>0 means the design contains fully aliased columns
 	{
 		trial=trial+1
 		aliased=0
-		gvec=genvec(m,n,q,k)
-		D=D_from_G(gvec,m,n,q,k)		
+		gmat=genmat(m,n,q,k)
+		#gvec=gmat_to_gvec(gmat,n,k)
+		D=D_from_G(gmat,m,n,q,k)		
 		outersuccess=1
 		final_success=0 #final_success=1 implies a required design was found
 		while (outersuccess!=0)  #outersuccess=1 implies that after a full round of iteration the criterion value decreased and a better design was found
@@ -134,36 +144,48 @@ mkssd=function(m,n,q,k,mef)
 			outer_avechi=ave_chisq(D,m,n,q)[[1]]
 			loop=0			
 			total=k*(n-1)*(n-2)/2
+			#pb <- winProgressBar(title = "progress bar", min = 0, max = total, width = 400)	#progress bar variable
 			pb <- txtProgressBar(min = 0, max = total, style=3)	#progress bar variable
 			i=1				
-			while (i<=(m-1) && (final_success==0) )
+			while (i<=k && (final_success==0) ) 
 			{
 				avechi=ave_chisq(D,m,n,q)[[1]]
 				if (mef*avechi<=L_chisq) final_success=1		
 				D1=D
-				min_avechi=avechi				
-				j=i+k			
-				while ((j<=m) && (final_success==0))
+				min_avechi=avechi
+				j=1							
+				while (j<=(n-2) && (final_success==0))
 				{
-					loop=loop+1 #loop is used for creating progress bar
-					Sys.sleep(0.1)
-   					setTxtProgressBar(pb, loop) #creates progress bar
-					if (D[1,i]!=D[1,j])
+					j1=j+1
+					while (j1<=(n-1) && (final_success==0))
 					{
-						temp_gvec=D[1,]					
-						temp_gvec[i]=D[1,j]
-						temp_gvec[j]=D[1,i]						
-						D_temp=Design_swap(D,m,n,k,i,j)
-						avechi_temp=ave_chisq(D_temp,m,n,q)[[1]]						
-						if (avechi_temp<min_avechi) 
+						loop=loop+1 #loop is used for creating progress bar
+						Sys.sleep(0.1)
+   						#setWinProgressBar(pb, loop, title=paste(round(loop*100/total, 0),"% done,","chisquare-eff=",L_chisq/min_avechi, "trial=", trial)) #creates progress bar
+						setTxtProgressBar(pb, loop) #creates progress bar
+						if (gmat[j,i]!=gmat[j1,i])
 						{
-							min_avechi=avechi_temp
-							D1=D_temp
-							success=1							
+							#temp_gmat=gmat					
+							#temp_gmat[j,i]=gmat[j1,i]
+							#temp_gmat[j1,i]=gmat[j,i]
+							#temp_gvec=gmat_to_gvec(temp_gmat,n,k)
+							col1=(j-1)*k+i #col1 and col2 are the corresponding columns in generator vector that are being interchanged
+							col2=(j1-1)*k+i
+							if (col1>m) col1=col1 %% m
+							if (col2>m) col2=col2%% m						
+							D_temp=Design_swap(D,m,n,k,col1,col2)
+							avechi_temp=ave_chisq(D_temp,m,n,q)[[1]]						
+							if (avechi_temp<min_avechi) 
+							{
+								min_avechi=avechi_temp
+								D1=D_temp
+								success=1							
+							}
+							if (mef*min_avechi<=L_chisq) final_success=1 #for terminating the outer while loop
 						}
-						if (mef*min_avechi<=L_chisq) final_success=1 #for terminating the outer while loop
+						j1=j1+1	
 					}
-					j=j+k				
+					j=j+1			
 				}
 				D=D1
 				i=i+1			
@@ -194,11 +216,11 @@ mkssd=function(m,n,q,k,mef)
 			if (genv[i]==1) genv[i]=-1
 			if (genv[i]==2) genv[i]=1
 		}
-		result=list(m,n,q,k,genv,Deff,t_taken, aliased)	
+		result=list(m=m,n=n,q=q,k=k,generator.vector=genv,design=D,efficiency=Deff,time.taken=t_taken, number.aliased.pairs=aliased)	
 	}
 	else
 	{
-		result=list(m,n,q,k,genv,Deff,max_chisq,t_taken, aliased)
+		result=list(m=m,n=n,q=q,k=k,generator.vector=genv,design=D,efficiency=Deff,max.chisq=max_chisq,time.taken=t_taken, number.aliased.pairs=aliased)
 	}
-	return(result)	
+	return(result)
 }
